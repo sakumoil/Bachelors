094b36c239333f16a5123b4b93e7327b5986c92e
Saku Moilanen <saku.moilanen1@gmail.com>
2019-09-04 15:59:51 +0300

    fix conversion on windows

--- convert_mat.m
+++ convert_mat.m
@@ -1,15 +1,17 @@
 % Purpose of this script is to convert images into a .mat -file using FlirMovieReader
 
 
-% folder_name = uigetdir('Folder with images');
-folder_name = '/Users/sakumoil/Projects/Bachelors/helsinki thermal/1/Kontrolli1';
+folder_name = uigetdir('Folder with images');
+% folder_name = '/Users/sakumoil/Projects/Bachelors/helsinki thermal/1/Kontrolli1';
 list_images = dir([folder_name, '/*.jpg']);
 for i = 1:length(list_images)
-    image_name = [folder_name, '/', list_images(i).name];
-    converted_name = [folder_name, '/', 'mat_', list_images(i).name, '.mat'];
+    split_imname = strsplit(list_images(i).name, '.');
+    split_imname = char(split_imname(1));
+    image_name = [folder_name, '\', list_images(i).name];
+    new_name = [folder_name, '\', split_imname, '.mat'];
     [v]= FlirMovieReader([image_name]);
     v.unit = 'temperatureFactory';
     [image, ~] =step(v);
-    image(i)=double(image);
-    save(converted_name, 'image(i)')
+    image = double(image);
+    save(new_name, 'image', '-mat')
 end
