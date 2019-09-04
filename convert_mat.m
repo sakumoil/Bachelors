423c9ae3a797562844bd35769ec3f910a70c8c25
Saku Moilanen <saku.moilanen1@gmail.com>
2019-09-04 11:21:47 +0300

    fix image save

--- convert_mat.m
+++ convert_mat.m
@@ -6,10 +6,10 @@ folder_name = '/Users/sakumoil/Projects/Bachelors/helsinki thermal/1/Kontrolli1'
 list_images = dir([folder_name, '/*.jpg']);
 for i = 1:length(list_images)
     image_name = [folder_name, '/', list_images(i).name];
-    converted_name = [folder_name, '/', 'mat_', list_images(i).name, '.mat']
+    converted_name = [folder_name, '/', 'mat_', list_images(i).name, '.mat'];
     [v]= FlirMovieReader([image_name]);
     v.unit = 'temperatureFactory';
     [image, ~] =step(v);
     image(i)=double(image);
-    save(converted_name, image)
+    save(converted_name, 'image(i)')
 end
